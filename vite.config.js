8b955b6d0520f725ca55b53ef8569f10e748c3ef
Kate Kozlova <zxchibabchichi@yandex.ru>
2026-08-18 19:37:13 +0300

    Added SCSS

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: './src',
-  server: {
-    port: 3000,
-    open: true, 
-  },
-  build: {
-    outDir: "../dist",
-  }
-})
+    server: {
+        port: 3000,
+        open: true,
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
+});
