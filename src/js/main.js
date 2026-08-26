7ec41615dad06da48cc197e3727b1b7d0771d1a0
Kate Kozlova <zxchibabchichi@yandex.ru>
2026-08-27 02:43:21 +0300

    Created pricing:hero section

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ import '../assets/scss/main.scss';
 // components
 import {useTheme} from './components/theme';
 import {useBurger} from './components/burger';
-import {useInsightSlider} from './components/slider';
-import {useTestimonialsSlider} from './components/slider';
+import {useInsightSlider} from './components/home/slider';
+import {useTestimonialsSlider} from './components/home/slider';
 
 useTheme();
 useBurger();
